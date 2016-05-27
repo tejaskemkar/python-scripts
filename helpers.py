dd1089c238ab0ce4bc3f058347632d85e8e4c339
tejaskemkar <tejas.kemkar@infibeam.net>
2016-05-28 01:09:02 +0530

    commenting testing functions from helpers and using array print helper in candies

--- helpers.py
+++ helpers.py
@@ -1,15 +1,13 @@
 import re
 
+# a=[i for i in xrange(10)]
+
+# for x in xrange(10):
+# 	print_array_before(a, x)
 
 def print_array_before(arr, i):
     n = len(arr)
     arr = re.sub(r'\[|\]|\s', '', str(arr))
-    print [n, i]
+    # print [n, i]
     print arr
     print(((2 * n) - (2 * (n - i))) * ' ') + '^'
-
-
-a=[i for i in xrange(10)]
-
-for x in xrange(1,10):
-	print_array_before(a, x)
