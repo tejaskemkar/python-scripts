107952a648bbb4c47e92f983314d74ef40f6066f
tejaskemkar <tejas.kemkar@infibeam.net>
2016-05-29 23:33:28 +0530

    fixing print array function to consider individual element sizes

--- helpers.py
+++ helpers.py
@@ -2,11 +2,17 @@ import re
 
 
 def print_array_before(arr, i):
+    s = 0
     n = len(arr)
-    arr = re.sub(r'\[|\]|\s', '', str(arr))
+    for j in xrange(n):
+        if(j < i):
+            s += len(str(arr[j]))
+    s+=i
+    # print s
+    arrstr = re.sub(r'^\[|\]$|\s', '', str(arr))
     # print [n, i]
-    print arr
-    print(((2 * n) - (2 * (n - i))) * ' ') + '^'
+    print arrstr
+    print(s * ' ') + '^'
 
 
 def local_minima(arr, i):
@@ -33,7 +39,7 @@ def local_maxima(arr, i):
     return False
 
 
-# a = [5, 4, 4, 4, 6, 6, 6, 7, 8, 2, 5, 6, 3]
+# a = [5, -1, 4, 4, 4, -50, 6, 6, 6, 25, 7, 8, 2, 5, 6, 100, 3]
 
 # for x in xrange(len(a)):
 #     print_array_before(a, x)
